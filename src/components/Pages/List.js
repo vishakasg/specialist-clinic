6938c517cf73ef4f4d3ca330c5f21e8d68614d76
Vishaka <vishaka.vishsg@gmail.com>
2023-06-23 14:41:14 +0930

    changing repo

--- src/components/Pages/List.js
+++ src/components/Pages/List.js
@@ -4,10 +4,13 @@ const DoctorsListPage = () => {
 	const listPageParagraph = `
     We endeavour to provide our patients with the help and support they need. Our staff try their best to fulfil the needs of our patients and create a high-quality experience.
 	`;
+	// const doctorsList = 
 
 	return (
 		<DefaultLayout>
-			<h1>Doctors List Page</h1>
+			<h1>Our Team</h1>
+			<h3>{ listPageParagraph }</h3>
+
 		</DefaultLayout>
 	);
 };
